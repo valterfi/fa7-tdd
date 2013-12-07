99856050bd8e09e455edb3a7b2e31ac95704a373
Valterfi Oliveira <morpheuw@gmail.com>
2013-12-06 21:30:19 -0300

    retorna valor de V

--- ExercicioNumerosRomanos.rb
+++ ExercicioNumerosRomanos.rb
@@ -1,7 +1,11 @@
 class RomanosParser
 
 	def parser(valor)
-		1	
+		if(valor == "I")
+			return 1
+		else(valor == "V")
+			return 5
+		end
 	end
 
 end
@@ -14,9 +18,10 @@ describe RomanosParser do
 		expect(romanosParser.parser("I")).to eql 1
 	end
 
+	it "deve retornar numero arabico de V" do
+
+		romanosParser = RomanosParser.new
+		expect(romanosParser.parser("V")).to eql 5
+	end
+
 end
-
-
-
-
-
