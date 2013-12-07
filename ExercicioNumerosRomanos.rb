a2612700a8ba8dd46dd6147b086c8d2b606615be
Valterfi Oliveira <morpheuw@gmail.com>
2013-12-06 21:44:46 -0300

    teste com todos os valores default

--- ExercicioNumerosRomanos.rb
+++ ExercicioNumerosRomanos.rb
@@ -5,8 +5,16 @@ class RomanosParser
 			return 1
 		elsif(valor == "V")
 			return 5
-		else(valor == "X")
+		elsif(valor == "X")
 			return 10
+		elsif(valor == "L")
+			return 50
+		elsif(valor == "C")
+			return 100
+		elsif(valor == "D")
+			return 500
+		else(valor == "M")
+			return 1000
 		end
 	end
 
@@ -28,6 +36,19 @@ describe RomanosParser do
 
 	it "deve retornar numero arabico de X" do
 		expect(@romanosParser.parser("X")).to eql 10
+	end
+
+	it "deve retornar numero arabico de L" do
+		expect(@romanosParser.parser("L")).to eql 50
+	end
+	it "deve retornar numero arabico de C" do
+		expect(@romanosParser.parser("C")).to eql 100
+	end
+	it "deve retornar numero arabico de D" do
+		expect(@romanosParser.parser("D")).to eql 500
+	end
+	it "deve retornar numero arabico de M" do
+		expect(@romanosParser.parser("M")).to eql 1000
 	end
 
 end
